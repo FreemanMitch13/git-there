dbc73ee7a594ba049c1c6b68f8b207be297a45c3
Christopher Ponzio <chrisponz11@yahoo.com>
2022-02-09 21:33:42 -0500

    added google api to current locator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,11 @@
-
 var googleLat ;
 var googleLon ;
-
-
-
 //Open Weather API Key
 const weatherApiKey = "1ad4965b69077cc77a3ec509434002cd";
-
 //Google API Key
-
 const mapApiKey = "AIzaSyCKB4f8DEdLTxjvjdKe2j19VtWeW02ghX4"
-
 var div = document.getElementById("location");
+
 function getLocation(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition,showError);
@@ -22,9 +16,18 @@ function getLocation(){
 function showPosition(position) {
     googleLat = position.coords.latitude;
     googleLon = position.coords.longitude;
-    
-    console.log(googleLat);
-    console.log(googleLon);
+    function getGoogle() {
+            var googleUrl = `https://maps.googleapis.com/maps/api/directions/JSON?&origin=${googleLat},${googleLon}&key=${mapApiKey}`;
+            fetch(googleUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data){
+                console.log(data);
+        })
+    }
+   getGoogle();
+   
 }
 function showError(error){
     if(error.PERMISSION_DENIED){
@@ -32,38 +35,39 @@ function showError(error){
     }
 }
 getLocation();
+console.log(googleLat);
+console.log(googleLon);
+// const mapApiKey = "AIzaSyCKB4f8DEdLTxjvjdKe2j19VtWeW02ghX4";
 
-const mapApiKey = "AIzaSyCKB4f8DEdLTxjvjdKe2j19VtWeW02ghX4";
-
-//api to get lat and lon
-function getApi(search) {
-    var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`;
-    fetch(requestUrl)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        cityNameValue = data[0]["name"];
-        console.log(data);
-        getLatLon(data[0]);
-    })
-    .catch( error => console.log(error) )
-    .finally( console.log('finished with fetch') )
-}
+// //api to get lat and lon
+// function getApi(search) {
+//     var requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=1&appid=${apiKey}`;
+//     fetch(requestUrl)
+//     .then(function (response){
+//         return response.json();
+//     })
+//     .then(function (data){
+//         cityNameValue = data[0]["name"];
+//         console.log(data);
+//         getLatLon(data[0]);
+//     })
+//     .catch( error => console.log(error) )
+//     .finally( console.log('finished with fetch') )
+// }
 
 
-function getLatLon(location) {
-    var { lat, lon } = location;
-    var city = location.name;
-    var latLonUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}`;
-}
+// function getLatLon(location) {
+//     var { lat, lon } = location;
+//     var city = location.name;
+//     var latLonUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&appid=${apiKey}`;
+// }
 
-function getGoogle() {
-    var googleLat ;
-    var googleLon ;
-    var googleUrl = `https://maps.googleapis.com/maps/api/directions/JSON?&origin=${googleLat},${googleLon}&key=${mapApiKey}`;
-    fetch(googleUrl)
-    .then(function (response) {
-        console.log(response);
-    })
-}
+// function getGoogle() {
+//     var googleLat ;
+//     var googleLon ;
+//     var googleUrl = `https://maps.googleapis.com/maps/api/directions/JSON?&origin=${googleLat},${googleLon}&key=${mapApiKey}`;
+//     fetch(googleUrl)
+//     .then(function (response) {
+//         console.log(response);
+//     })
+// }
